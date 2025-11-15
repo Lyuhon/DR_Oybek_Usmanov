0b091697ac210c0912dc30b30db9de876234bac4
Lyuhon <aezakmi22990@gmail.com>
2025-11-16 01:58:17 +0500

    added useeffect

--- app/ContactForm.jsx
+++ app/ContactForm.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ArrowRight, ChevronDown } from 'lucide-react';
 
 export default function ContactForm({ t }) {
